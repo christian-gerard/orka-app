2b500110c2f229113abfd06fb774d69bc8fc38a0
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-09-08 19:16:03 -0600

    Updates the orka auth

--- server/server/models/User.py
+++ server/server/models/User.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    
+
     email = models.EmailField(
         max_length=100,
         unique=True
@@ -15,7 +15,6 @@ class User(models.Model):
         max_length= 100
     )
 
-    
     
     def __str__(self):
         return self.email
