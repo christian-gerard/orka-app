8b3293f96d6f62b6009b87beb6edcad2df994000
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-09-08 19:14:12 -0600

    Adding Account + User Models

--- server/server/models/Account.py
+++ server/server/models/Account.py
@@ -1,11 +1,10 @@
 from django.db import models
 
-
-class User(models.Model):
-    email = models.EmailField(
-        max_length=100,
-        unique=True
+class Account(models.Model):
+    
+    account_name = models.CharField(
+        max_length=100
     )
     
     def __str__(self):
-        return self.email
+        return self.account_name
--- server/server/models/User.py
+++ server/server/models/User.py
@@ -1,10 +1,21 @@
 from django.db import models
 
 class User(models.Model):
+    
     email = models.EmailField(
         max_length=100,
         unique=True
     )
+
+    first_name = models.CharField(
+        max_length=100
+    )
+
+    last_name = models.CharField(
+        max_length= 100
+    )
+
+    
     
     def __str__(self):
         return self.email
