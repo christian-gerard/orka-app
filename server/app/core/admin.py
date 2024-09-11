dc03bc3f8d0f457dafb9502b17605c1db289eb77
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-09-10 20:26:11 -0600

    Updates Orka admin pages

--- server/app/core/admin.py
+++ server/app/core/admin.py
@@ -1,3 +1,48 @@
-from django.contrib import admin # noqa
+"""
+Django Admin Customization
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users"""
+    ordering = ['id']
+    list_display = ['email', 'first_name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields':(
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }
+        ),
+        (_('Important Dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'first_name',
+                'last_name',
+                'is_active',
+                'is_staff',
+                'is_superuser'
+            )
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
