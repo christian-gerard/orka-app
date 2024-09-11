5a63b2783f37133a1c7c9209ae52f152463e0de9
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-09-10 19:31:32 -0600

    Updates the user model

--- server/app/core/models.py
+++ server/app/core/models.py
@@ -1,3 +1,37 @@
-from django.db import models # noqa
+"""
+Database Models
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+
+class UserManager(BaseUserManager):
+    """Manager for Users"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create Save Return a New User"""
+        if not email:
+            raise ValueError('User must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser,PermissionsMixin):
+    """User in the system"""
+
+    email = models.EmailField(max_length=250, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
