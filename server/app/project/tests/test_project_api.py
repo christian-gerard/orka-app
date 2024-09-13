40d87c44a1fd9930d6363fc209a07d23c1ad14f4
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-09-13 12:13:55 -0600

    Updating

---
+++ server/app/project/tests/test_project_api.py
@@ -0,0 +1,13 @@
+"""
+Tests for Project API
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Project
+
+from project.serializers import ProjectSerializer
