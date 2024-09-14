0390137dba710a44d43143c84dc3697f578b3764
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-09-13 19:35:45 -0600

    Updating Account Endpoint to include clients

--- server/app/account/tests/test_account_api.py
+++ server/app/account/tests/test_account_api.py
@@ -21,6 +21,10 @@ import pdb
 
 ACCOUNT_URL = reverse("account:account-list")
 
+CLIENT_URL = reverse("account:account-list") + "clients/"
+
+pdb.set_trace()
+
 def detail_url(account_id):
     """Return detail url for specific account"""
     return reverse('account:account-detail', args=[account_id])
@@ -113,4 +117,7 @@ class PrivateAccountAPITests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
+
+    def test_get_client_list(self):
+        """Test Client Details"""
 
