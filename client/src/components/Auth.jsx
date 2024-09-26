da6b4ea72350d00ec31451104e95136e33f26a8a
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-09-26 16:22:47 -0600

    Working on New User form

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -97,22 +97,39 @@ function Auth() {
         newUser ?
         // Account Creation
         <div className='bg-black text-white py-12 px-8 flex flex-col items-center '>
-          <h1 className='text-4xl p-2 tracking-[0.8em] reddit-mono italic'> ORKA </h1>
-          <h3>Signup for an account</h3>
+          <h1 className='text-5xl p-2 tracking-[0.8em] reddit-mono justify-center pl-[0.8em] '> ORKA </h1>
+          <h2 className='text-2xl tracking-[0.2em]'>Sign Up</h2>
           <form onSubmit={formik.handleSubmit} className='flex flex-col p-2'>
             <div className='flex flex-row'>
               <div className='flex flex-col mr-2'>
                 <div className='flex flex-col'>
-                  <label htmlFor="username" className='text-xl'>Email</label>
+                  <label htmlFor="first_name" className='text-lg'>First Name</label>
+                  <input id="first_name"
+                      name="first_name"
+                      type="first_name"
+                      onChange={formik.handleChange}
+                      value={formik.values.firstName}
+                      className='text-black my-2 p-1 text-lg'
+                      placeholder='First Name'/>
+
+                  <label htmlFor="first_name" className='text-lg'>Last Name</label>
+                  <input id="last_name"
+                      name="last_name"
+                      type="last_name"
+                      onChange={formik.handleChange}
+                      value={formik.values.lastName}
+                      className='text-black my-2 p-1 text-lg'
+                      placeholder='Last Name'/>
+                  <label htmlFor="username" className='text-lg'>Email</label>
                   <input id="email"
                     name="email"
                     type="email"
                     onChange={formik.handleChange}
                     value={formik.values.email}
                     className='text-black my-2 p-1 text-lg'
-                    placeholder='email'/>
+                    placeholder='Email'/>
                 </div>
-                <label htmlFor="password" className='text-xl'>Password</label>
+                <label htmlFor="password" className='text-lg'>Password</label>
                 <input id="password"
                   name="password"
                   type="password"
@@ -120,43 +137,25 @@ function Auth() {
                   value={formik.values.password}
                   className='text-black my-2 p-1 text-lg'
                   placeholder='password'/>
-                <label htmlFor="password" className='text-xl'>Confirm Password</label>
                 <input id="password"
                   name="passwordConfirmation"
-                  type="password"
+                  type="Password"
                   onChange={formik.handleChange}
                   value={formik.values.passwordConfirmation}
                   className='text-black my-2 p-1 text-lg'
-                  placeholder='Rewrite Password'/>
+                  placeholder='Confirm Password'/>
               </div>
-              <div className='flex flex-col ml-2'>
-                <label htmlFor="first_name" className='text-xl'>First Name **</label>
-                <input id="first_name"
-                    name="first_name"
-                    type="first_name"
-                    onChange={formik.handleChange}
-                    value={formik.values.first_name}
-                    className='text-black my-2 p-1 text-lg'
-                    placeholder='First Name'/>
 
-                <label htmlFor="first_name" className='text-xl'>Last Name **</label>
-                <input id="last_name"
-                    name="last_name"
-                    type="last_name"
-                    onChange={formik.handleChange}
-                    value={formik.values.lastName}
-                    className='text-black my-2 p-1 text-lg'
-                    placeholder='Last Name'/>
-              </div>
+
             </div>
             <button type="submit" className='mt-4 bg-white text-black'>Create Account</button>
           </form>
-          <button type='button' className='mt-6 border border-white p-1 text-lg w-full underline' onClick={handleNewUser}>Log In</button>
+          <button type='button' className='mt-2 border border-white p-1 text-md w-full underline' onClick={handleNewUser}>Log In</button>
         </div>
         :
         // Login
         <div className='bg-black text-white py-12 px-8 flex flex-col'>
-            <h1 className='text-4xl p-2 tracking-[0.8em] reddit-mono italic w-full flex justify-center'> ORKA </h1>
+            <h1 className='text-5xl p-2 tracking-[0.8em] reddit-mono  w-full flex justify-center pl-[0.8em]'> ORKA </h1>
             <form onSubmit={formik.handleSubmit} className='flex flex-col p-2'>
                 <label htmlFor="username" className='text-xl'>Email</label>
                 <input
@@ -179,7 +178,7 @@ function Auth() {
                     placeholder='password'
                 />
                 <button type="submit" className='mt-4 bg-white text-black'>Log In</button>
-                <button type='button' className='mt-6 border border-white p-1 text-lg w-full underline' onClick={handleNewUser}>Create New User</button>
+                <button type='button' className='mt-2 border border-white p-1 text-md w-full underline' onClick={handleNewUser}>Create New User</button>
             </form>
         </div>
       }
