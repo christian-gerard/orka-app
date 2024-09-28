047efee7f535a4721b1c645767961dc6c95d54de
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-09-27 22:04:48 -0600

    Account Page Updates

--- client/src/pages/Accounts.jsx
+++ client/src/pages/Accounts.jsx
@@ -2,8 +2,24 @@
 
 function Accounts() {
 
+    const renderAccounts = () => {
+        return <h1>RENDER ACCOUNTS HERE</h1>
+    }
+
     return(
-        <h1>ACCOUNTS</h1>
+        <div className='h-full w-full bg-ocean '>
+            <div>
+                <p className='text-3xl'>Account Selection</p>
+                <p></p>
+            </div>
+
+            <div>
+                {
+                    renderAccounts()
+                }
+            </div>
+
+        </div>
     )
 }
 
