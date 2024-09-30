fdf55dff7d3fa9d1fbe2c8c6690443fc1ce944b4
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-09-30 17:17:31 -0600

    Cleaning Up FrontEnd for the day

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -9,8 +9,35 @@ function Settings() {
                 <p className='text-2xl'>General</p>
 
                 <div className='flex flex-row justify-between my-2'>
+
                     <div className='text-xl flex flex-col'>
                         <p>Setting</p>
+                        <p className='text-sm italic'>Description of what the setting is </p>
+                    </div>
+
+                    <div>
+                        <button className='border bg-black text-white h-full items-center p-1 rounded-lg'>Setting</button>
+                    </div>
+
+                </div>
+
+                <div className='flex flex-row justify-between my-2'>
+
+                    <div className='text-xl flex flex-col'>
+                        <p>Dark Mode</p>
+                        <p className='text-sm italic'>Description of what the setting is </p>
+                    </div>
+
+                    <div>
+                        <button className='border bg-black text-white h-full items-center p-1 rounded-lg'>Setting</button>
+                    </div>
+
+                </div>
+
+                <div className='flex flex-row justify-between my-2'>
+
+                    <div className='text-xl flex flex-col'>
+                        <p>Account</p>
                         <p className='text-sm italic'>Description of what the setting is </p>
                     </div>
 
