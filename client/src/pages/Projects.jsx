dab8cb29fa3a35f4b0ebe8eb9f3f83f6a358dd9a
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-09-30 11:21:36 -0600

    Working on Project Page

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,12 +1,18 @@
+import { useState, useContext } from 'react'
 import Project from '../components/Project'
+import CloseIcon from '@mui/icons-material/Close';
 
 function Projects() {
+    const [newProject, setNewProject] = useState(false)
+
+    const handleNewProject = () => setNewProject(!newProject)
+
     return (
         <div className='h-full w-full'>
             {/* Page Header 10%*/}
             <div className='h-[5%] w-full text-3xl flex justify-between items-center'>
                 <p className=''>Projects</p>
-                <div className='border bg-black'>
+                <div className='border bg-black' onClick={handleNewProject}>
                     <p className='text-lg text-white p-1'>+ New Project</p>
                 </div>
             </div>
@@ -14,7 +20,7 @@ function Projects() {
 
 
             {/* Projects */}
-            <div className='h-[95%] w-full border overflow-scroll'>
+            <div className='h-[95%] w-full border scrollbar scrollbar-thumb-ocean overflow-scroll'>
                 <Project />
                 <Project />
                 <Project />
@@ -22,6 +28,20 @@ function Projects() {
                 <Project />
             </div>
 
+            {
+                newProject ?
+
+                <div className='fixed inset-0 flex flex-col justify-center items-center transition-colors backdrop-blur'>
+                    <div className='bg-white border'>
+                        <CloseIcon onClick={handleNewProject} />
+                    </div>
+                </div>
+
+                :
+
+                <>
+                </>
+            }
 
 
         </div>
