00d7bb3e8cf3ed4a96def2cf4b2106c03a5e764b
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-09-30 17:08:44 -0600

    Updating the Dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -13,23 +13,25 @@ function Dashboard() {
             <div className='h-[45%] w-full border'>
 
 
-                <p>Outstanding Tasks</p>
-                <div className=''>
-                    <Task />
-                    <Task />
-                    <Task />
-                    <Task />
-                    <Task />
-                    <Task />
-                    <Task />
-                    <Task />
-                    <Task />
-                    <Task />
-                    <Task />
-                    <Task />
-                    <Task />
-                    <Task />
-                    <Task />
+                <p className='h-[5%]'>Outstanding Tasks</p>
+                <div className='h-[95%] overflow-scroll-y scrollbar scrollbar-thumb-ocean overflow-scroll'>
+                    <div>
+                        <Task />
+                        <Task />
+                        <Task />
+                        <Task />
+                        <Task />
+                        <Task />
+                        <Task />
+                        <Task />
+                        <Task />
+                        <Task />
+                        <Task />
+                        <Task />
+                        <Task />
+                        <Task />
+                        <Task />
+                    </div>
                 </div>
 
 
@@ -40,12 +42,50 @@ function Dashboard() {
             <div className='h-[50%] w-full flex flex-row'>
                 {/* Clients */}
                 <div className='w-[50%] h-full border'>
-                    <p>Clients</p>
+                    <p className='h-[5%]'>Clients</p>
+                    <div className='h-[95%] overflow-scroll-y scrollbar scrollbar-thumb-ocean overflow-scroll'>
+                        <div>
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                        </div>
+                </div>
 
                 </div>
                 {/* Projects */}
                 <div className='w-[50%] h-full border'>
-                    <p>Projects</p>
+                    <p className='h-[5%]'>Projects</p>
+                    <div className='h-[95%] overflow-scroll-y scrollbar scrollbar-thumb-ocean overflow-scroll'>
+                        <div>
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                        </div>
+                </div>
 
                 </div>
             </div>
