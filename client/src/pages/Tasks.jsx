787efb2c7f268f37b0f156c9379f00b31c4616c9
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-10-09 11:46:10 -0600

    New Task Dropdown being put up

--- client/src/pages/Tasks.jsx
+++ client/src/pages/Tasks.jsx
@@ -1,11 +1,18 @@
 import { useState, useEffect, useContext } from 'react'
 import { UserContext } from '../context/UserContext'
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import Task from '../components/Task'
 
 function Tasks({id, name, deadline, description, project_type, budget}){
 
     const { token } = useContext(UserContext)
     const [currentTasks, setCurrentTasks] = useState(null)
+    const [newTask, setNewTask] = useState(false)
+
+    const handleNewTask = () => {
+        setNewTask(!newTask)
+    }
 
     useEffect(() => {
         fetch('/api/project/tasks', {
@@ -31,20 +38,31 @@ function Tasks({id, name, deadline, description, project_type, budget}){
             {/* Page Header 10%*/}
             <div className='h-[5%] w-full text-3xl flex justify-between items-bottom'>
                 <p className=''>Tasks</p>
-                <div>
-                    <p>Add Task</p>
-                </div>
             </div>
 
             {/* main body */}
             <div className='h-[95%] w-full flex flex-col gap-4 border scrollbar-thin scrollbar-thumb-ocean overflow-scroll'>
 
                 <div>
-                    <div>
-                        My Tasks
+                    <div className='flex flex-row justify-between items-center'>
+                        <p>My Tasks</p>
+                        <p className='flex items-center' onClick={handleNewTask}>
+                            { newTask ? <KeyboardArrowDownIcon /> : <KeyboardArrowRightIcon /> }
+                            Add Task
+                        </p>
                     </div>
 
                     <div>
+                        {
+                            newTask ?
+
+                            <div className='bg-ocean w-full h-[40px]'>
+                                <h1>NEW TASK IN PUT</h1>
+                            </div>
+                            :
+                            <>
+                            </>
+                        }
                         {
                             currentTasks ?
 
