07b23b56e39ecdaad06dad982d2a56e426a359b8
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-10-11 16:07:48 -0600

    Working on Stuff

--- client/src/components/Account.jsx
+++ client/src/components/Account.jsx
@@ -2,9 +2,10 @@
 
 function Account({name, users}){
     return(
-        <div className='flex flex-col justify-center'>
-            <div className='rounded-[100%] h-[100px] w-[100px] border bg-ocean'></div>
-            <p className='w-full text-center text-xl'>{name ? name : 'Unnamed'} ({users ? users.length : "0"})</p>
+        <div className='flex flex-col justify-center items-center'>
+            <div className='rounded-[100%] size-[100px] border bg-ocean'></div>
+            <p className='text-lg mt-2'>{name ? name : 'Unnamed'} </p>
+            <p>({users ? users.length : "0"} Active Users)</p>
         </div>
     )
 }
