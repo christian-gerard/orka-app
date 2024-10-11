f627b8347c040128282cb9bffdcdc57a4ad7b3d4
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-10-11 12:52:14 -0600

    Mobile styling for projects
    "
    "

--- client/src/components/Project.jsx
+++ client/src/components/Project.jsx
@@ -276,16 +276,16 @@ function Project({id, name, deadline, description, project_type}) {
 
                     {/* Project Box Header */}
                     <div className='flex flex-row justify-between h-[20%] border-b'>
-                        <p className='text-2xl'>{name ? name : 'Untitled'}</p>
-                        <p className='text-lg'>{deadline ? deadline : 'No Deadline'}</p>
+                        <p className='text-[0.8em] sm:text-xl'>{name ? name.slice(0,20) : 'Untitled'}</p>
+                        <p className='text-[0.8em] sm:text-lg'>{deadline ? deadline.slice(5,12) : 'No Deadline'}</p>
                     </div>
 
                     {/* Project Details */}
                     <div className='flex flex-col justify-between h-[80%]'>
-                        <p className='scrollbar overflow-scroll text-sm h-[70%]'>
-                            {description ? description : "No Description Available"}
+                        <p className='scrollbar overflow-scroll text-[0.8em] sm:text-lg h-[75%]'>
+                            {description ? description.slice(0,125) : "No Description Available"}
                         </p>
-                        <p className='h-[30%] text-lg'>
+                        <p className='h-[25%] text-[0.8em] sm:text-lg'>
                             {project_type ? project_type : 'No Type'}
                         </p>
                     </div>
