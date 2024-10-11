14a703ade3301b30444334abf1d87e1a33c0e9f9
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-10-10 19:08:36 -0600

    WOrking On Task Post

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -1,18 +1,40 @@
 import { useContext, useEffect, useState } from 'react'
+import { UserContext } from '../context/UserContext'
 import Close from '@mui/icons-material/Close';
 import CloseIcon from '@mui/icons-material/Close';
 
 function Task({deadline, description, category, status, project}) {
+    const { projects, updateProjects, tasks } = useContext(UserContext)
 
     const handleStatus = () => {
         console.log("Handle Status Triggered")
     }
 
+    const handleTaskDelete = () => {
+        console.log("DELETING TASK")
+    }
+
+    useEffect(() => {
+        updateProjects()
+    },[])
+
+
+    console.log(projects.filter(proj => proj.id === project))
+
     return (
         <div className='p-1 m-2 border flex flex-row justify-between items-center h-[40px]'>
 
             <div className='w-[50%] flex flex-row items-center'>
-                <p className='bg-ocean border border-black text-white p-[0.1em]'>{project ? project : "No Project"}</p>
+                <div onClick={handleTaskDelete}>
+                    <CloseIcon />
+                </div>
+                <p className='bg-ocean border border-black text-white p-[0.1em]'>
+                    {projects ?
+                       "TESTING"
+                        :
+                        "No Project"
+                    }
+                </p>
                 <p>{description ? description : "No Description"}</p>
             </div>
 
