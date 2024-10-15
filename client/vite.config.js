40984ebe8054c36f1bb638921f2be3b01c0009a9
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-10-15 08:16:07 -0600

    Reconfig for Vite URL

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
 
     proxy: {
-      '/api': 'http://ec2-184-72-8-99.us-west-1.compute.amazonaws.com',
+      '/api': 'http://localhost:8000',
     },
 
 
