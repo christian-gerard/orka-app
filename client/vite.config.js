c74556c0faa12f489b016b08c6f8e85b073b6f38
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-10-15 08:22:42 -0600

    Orka

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-
-    proxy: {
-      '/api': 'http://localhost:8000',
-    },
-
-
-  },
-  build: {
-    outDir: 'dist',  // This should be the same as the directory in your YAML file
-  },
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    // Development mode: enable proxy
+    return {
+      server: {
+        proxy: {
+          '/api': 'http://localhost:8000',
+        },
+      },
+      build: {
+        outDir: 'dist',
+      },
+      plugins: [react()],
+    }
+  } else {
+    // Production mode: no proxy
+    return {
+      build: {
+        outDir: 'dist',
+      },
+      plugins: [react()],
+    }
+  }
 })
