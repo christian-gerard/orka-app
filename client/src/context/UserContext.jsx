7197ea8decc87275795c9aa4f3bb0ab5914c173b
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-10-25 20:48:46 -0600

    Updating Everything for CORS ETC

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import Cookies from 'js-cookie'
 
-const API_URL = import.meta.env.API_URL
+const API_URL = import.meta.env.VITE_API_URL
 
 export const UserContext = createContext()
 
