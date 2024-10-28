d807b3c784394d27a6b42c8462721a7fb3da13f3
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-10-28 17:49:29 -0600

    Project User Edit

---
+++ client/src/components/User.jsx
@@ -0,0 +1,20 @@
+
+function User({id, first_name, last_name, email}) {
+
+    return (
+        <div className='flex flex-row border items-center justify-between'>
+
+
+            <div className='w-[40%] px-2 text-md sm:text-xl bold flex flex-nowrap flex-row justify-end'>
+                <p>{first_name} {last_name}</p>
+            </div>
+            <div className='w-[60%] px-2 bg-black text-white text-md sm:text-lg'>
+                <p className='truncate'>{email}</p>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default User
--- server/app/project/serializers.py
+++ server/app/project/serializers.py
@@ -28,7 +28,7 @@ class BudgetSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Budget
-        fields = ['budget_name', 'amount', 'description', 'category', 'project']
+        fields = ['budget_name', 'amount', 'category', 'project']
         read_only_fields = ["id"]
 
 
