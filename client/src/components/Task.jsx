e43885dbf60ca7cb18a93d9bea1b1a8f7a95e9f2
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-11-02 15:17:37 -0600

    Working on Task Styling

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -3,15 +3,42 @@ import { UserContext } from '../context/UserContext'
 import { toast } from 'react-hot-toast'
 import { object, string, array, number, bool } from "yup";
 import { useFormik, Formik, Form, Field } from 'formik'
-import Close from '@mui/icons-material/Close';
+import Message from '../components/Message'
+import User from '../components/User'
+import DeleteIcon from '@mui/icons-material/Delete';
 import CloseIcon from '@mui/icons-material/Close';
+import EditIcon from '@mui/icons-material/Edit';
 import axios from 'axios'
 
 function Task({id, deadline, description, category, status, project, users}) {
+
     const { API_URL, accessToken } = useContext(UserContext)
     const oldStatus = status
     const [isChecked, setIsChecked] = useState(false)
     const [isOpen, setIsOpen] = useState(false)
+    const [editTask, setEditTask] = useState(false)
+
+    const token = accessToken
+
+    const handleEditTask = () => {
+        setEditTask(!editTask)
+    }
+
+    const handleTaskDelete = () => {
+        axios.delete(`${API_URL}/api/tasks/${id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then(resp => {
+            if(resp.status == 204) {
+
+                toast.success('Deleted')
+            } else {
+                toast.error('Error Occured')
+            }
+        })
+    }
 
     const handleMarkAsDone = () => {
 
@@ -81,7 +108,7 @@ function Task({id, deadline, description, category, status, project, users}) {
 
     const userSchema = object({
         user: array()
-      });
+    });
 
     const initialValues = {
         user: []
@@ -115,21 +142,18 @@ function Task({id, deadline, description, category, status, project, users}) {
 
     },[])
 
-
-
-
     return (
 
-            <div  className={`p-1 border flex flex-col text-sm ${isChecked && 'text-gray'} ${isOpen ? 'h-[300px]' : 'h-[40px]'}`}>
-                <div className=' w-full flex flex-row justify-between items-center'>
+        <div  className={`transition-all duration-800 ease-in-out p-1 border hover:duration-200 hover:border-[3px] flex flex-col text-sm ${isChecked && 'text-gray'} ${isOpen ? 'h-screen max-h-[90%]' : 'h-[40px] max-h-[40px]'}`}>
+                <div className='w-full flex flex-row justify-between items-center' onClick={handleOpen}>
 
                     <div className='w-[70%] flex flex-row items-center gap-2 ' >
                         { isChecked ?
-                            <div className='peer w-[25px] h-[25px] border-2 text-2xl flex justify-center items-center' onClick={handleUnmark}> ✓ </div>
+                            <div className='peer w-[25px] h-[25px] border-2 text-2xl flex justify-center items-center bg-ocean' onClick={handleUnmark}> ✓ </div>
                             :
-                            <div className='peer w-[25px] h-[25px] border' onClick={handleMarkAsDone}></div>
+                            <div className='peer w-[25px] h-[25px] border bg-white' onClick={handleMarkAsDone}></div>
                         }
-                        <p onClick={handleOpen} className='hover:bg-ocean truncate'>{description ? description : "No Description"}</p>
+                        <p  className='truncate'>{description ? description : "No Description"}</p>
                     </div>
 
                     <div className='w-[30%] flex flex-row justify-end items-center gap-2'>
@@ -141,57 +165,101 @@ function Task({id, deadline, description, category, status, project, users}) {
                 </div>
 
 
-            {
-                isOpen &&
+                <div className={`overflow-hidden w-full text-sm h-full mt-1 `}>
 
-                <div className={`transition-all duration-500 transform origin-top overflow-hidden text-sm h-[250px] `}>
-                    <label className='ml-2 text-base '> Users </label>
+                    {/* Header */}
+                    <div className='flex flex-row justify-between p-1 bg-black text-white'>
+                        <p>More Details</p>
+                        <div>
+                            <DeleteIcon onClick={handleTaskDelete} />
+                            <EditIcon onClick={handleEditTask}/>
+                        </div>
+                    </div>
+                    {/* More Details */}
+                    <div className='flex flex-row w-full'>
+                        <div className='w-[50%]'>
+                            <div className='text-base'> Users </div>
+                            <div className='border h-[100px]'>
+                                <User />
+                            </div>
+                        </div>
 
-                    <Formik
-                    onSubmit={formik.handleSubmit}
-                    initialValues={initialValues}
-                    >
-                        <Form
-                        className='h-full w-full border p-2 flex flex-col gap-2 '
+                        <div className='w-[50%]'>
+                            <div className='ml-2 text-base'> Other Details </div>
+                            <div className='border w-full'>
+                                <p>{project && project.name}</p>
+                                <p>{deadline && deadline.slice(5,10)}</p>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Thread */}
+                    <div className=''>
+                        <p>Thread</p>
+                        <div className='h-[600px] overflow-y-scroll scrollbar scrollbar-thumb-ocean scrollbar-track-gray border flex flex-col gap-4'>
+                            <Message outgoing={true} message={'Should we push the deadline back'}/>
+                            <Message outgoing={false} message={'Hello'}/>
+                            <Message outgoing={false} message={'Working on something good'}/>
+                            <Message outgoing={true} message={'Wellp'}/>
+                            <Message outgoing={false} message={'Working on something good'}/>
+                            <Message outgoing={true} message={'Wellp'}/>
+                            <Message outgoing={false} message={'Working on something good'}/>
+                            <Message outgoing={true} message={'Wellp'}/>
+                            <Message outgoing={false} message={'Working on something good'}/>
+                            <Message outgoing={true} message={'Wellp'}/>
+                        </div>
+                    </div>
+                    {
+                        editTask &&
+
+                        <Formik
                         onSubmit={formik.handleSubmit}
                         initialValues={initialValues}
                         >
-                            <Field
-                                name='user'
-                                as='select'
-                                multiple
-                                value={formik.values.user}
-                                onChange={(e) => {
-                                    const selectedUserId = parseInt(e.target.value); // Get the selected user ID
-                                    const currentUsers = formik.values.user;
+                            <Form
+                            className='absolute h-[90%] inset-0 flex flex-col justify-center items-center transition-colors backdrop-blur-sm'
+                            onSubmit={formik.handleSubmit}
+                            initialValues={initialValues}
+                            >
+                                <CloseIcon onClick={handleEditTask}/>
 
-                                    // Toggle the selected user ID in the users array
-                                    if (currentUsers.includes(selectedUserId)) {
-                                        // If already selected, remove it
-                                        formik.setFieldValue(
-                                            'user',
-                                            currentUsers.filter((id) => id !== selectedUserId)
-                                        );
-                                    } else {
-                                        // If not selected, add it
-                                        formik.setFieldValue('user', [...currentUsers, selectedUserId]);
+                                <Field
+                                    name='user'
+                                    as='select'
+                                    multiple
+                                    value={formik.values.user}
+                                    onChange={(e) => {
+                                        const selectedUserId = parseInt(e.target.value); // Get the selected user ID
+                                        const currentUsers = formik.values.user;
+
+                                        // Toggle the selected user ID in the users array
+                                        if (currentUsers.includes(selectedUserId)) {
+                                            // If already selected, remove it
+                                            formik.setFieldValue(
+                                                'user',
+                                                currentUsers.filter((id) => id !== selectedUserId)
+                                            );
+                                        } else {
+                                            // If not selected, add it
+                                            formik.setFieldValue('user', [...currentUsers, selectedUserId]);
+                                        }
+
+                                    }}
+                                    onBlur={formik.handleBlur}
+                                    className='ml-2 mr-2 border scrollbar scrollbar-thumb-ocean'
+                                >
+                                    {
+                                        users && users.sort((a, b) => a.first_name.localeCompare(b.first_name)).map(user => <option key={user.id} className='text-sm border p-1 m-2' value={user.id}>{user.email}</option>)
                                     }
 
-                                }}
-                                onBlur={formik.handleBlur}
-                                className='ml-2 mr-2 border scrollbar scrollbar-thumb-ocean'
-                            >
-                                {
-                                    users && users.sort((a, b) => a.first_name.localeCompare(b.first_name)).map(user => <option key={user.id} className='text-sm border p-1 m-2' value={user.id}>{user.email}</option>)
-                                }
+                                </Field>
 
-                            </Field>
                                 <button type='submit' className={'bg-black text-white'}>Update</button>
                             </Form>
-                            </Formik>
+                        </Formik>
+                    }
 
                 </div>
-            }
+
         </div>
     )
 }
--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -6,10 +6,10 @@ function User({id, first_name, last_name, email}) {
 
 
             <div className='w-[40%] px-2 text-md sm:text-xl bold flex flex-nowrap flex-row justify-end'>
-                <p className='truncate'>{first_name} {last_name}</p>
+                <p className='truncate'>{first_name ? first_name : "XYZ Testing"} {last_name}</p>
             </div>
             <div className='w-[60%] px-2 bg-black text-white text-md sm:text-lg'>
-                <p className='truncate'>{email}</p>
+                <p className='truncate'>{email ? email : 'test@email.com'}</p>
             </div>
 
 
