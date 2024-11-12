860b1c471d0e3fd52349feffee7db7fae09d18ea
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-11-11 19:51:19 -0700

    UPDATES

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -84,49 +84,9 @@ function Settings() {
 
 
                                 <div className='h-[95%] w-full flex flex-col lg:gap-2 overflow-scroll scrollbar scrollbar-thumb-ocean'>
+
                                     <div className='flex flex-row gap-2'>
-                                        <div className='flex justify-center items-center'>
-                                            <Dropzone onDrop={acceptedFiles => {
-                                                setFiles(acceptedFiles.map(file => Object.assign(file, {
-                                                preview: URL.createObjectURL(file)
-                                                })));}
-                                            }>
-                                                {({getRootProps, getInputProps}) => (
-                                                    <div {...getRootProps()} >
-                                                    <input {...getInputProps()} />
-                                                    <p className='p-2' >
-                                                        {
-                                                            files[0] ?
-                                                            <img
-                                                                className='rounded-[100%] h-[125px] w-[125px] object-cover'
-                                                                src={files[0].preview}
-                                                                alt='client'
-                                                            />
-                                                            :
 
-
-
-
-                                                                <div className='rounded-[100%] h-[125px] w-[125px] bg-ocean flex items-center text-white'>
-                                                                    <UploadFileIcon style={{width:'35px', height:'35px'}} />
-                                                                    <div className='flex flex-col justify-center text-xs'>
-                                                                        <p className='text-sm'>Img Upload</p>
-                                                                        <p className='italic'>Drag or Click</p>
-                                                                    </div>
-                                                                </div>
-
-
-
-
-                                                        }
-
-                                                    </p>
-                                                    </div>
-
-                                                )}
-                                            </Dropzone>
-
-                                        </div>
 
                                         <div className='w-[60%] flex flex-col justify-end'>
                                             <label className='ml-2'> First Name </label>
@@ -157,33 +117,73 @@ function Settings() {
                                             {formik.errors.last_name && formik.touched.last_name && (
                                                 <div className="text-sm text-red ml-2"> **{formik.errors.last_name}</div>
                                             )}
-                                            </div>
+                                        </div>
 
+                                    </div>
+
+                                    <div className='flex flex-row gap-2'>
+                                        <div className='flex justify-center items-center'>
+                                            <Dropzone onDrop={acceptedFiles => {
+                                                setFiles(acceptedFiles.map(file => Object.assign(file, {
+                                                preview: URL.createObjectURL(file)
+                                                })));}
+                                            }>
+                                                {({getRootProps, getInputProps}) => (
+                                                    <div {...getRootProps()} >
+                                                    <input {...getInputProps()} />
+                                                    <p className='p-2' >
+                                                        {
+                                                            files[0] ?
+                                                            <img
+                                                                className='rounded-[100%] h-[125px] w-[125px] object-cover'
+                                                                src={files[0].preview}
+                                                                alt='client'
+                                                            />
+                                                            :
+                                                                <div className='rounded-[100%] h-[125px] w-[125px] bg-ocean flex items-center text-white'>
+                                                                    <UploadFileIcon style={{width:'35px', height:'35px'}} />
+                                                                    <div className='flex flex-col justify-center text-xs'>
+                                                                        <p className='text-sm'>Img Upload</p>
+                                                                        <p className='italic'>Drag or Click</p>
+                                                                    </div>
+                                                                </div>
+
+
+
+
+                                                        }
+
+                                                    </p>
+                                                    </div>
+
+                                                )}
+                                            </Dropzone>
+
+                                        </div>
+
+                                        <div className='w-[60%] flex flex-col justify-end'>
+                                                <label className='ml-2'> Email </label>
+                                                <Field
+                                                    name='name'
+                                                    value={formik.values.email}
+                                                    onChange={formik.handleChange}
+                                                    type='text'
+                                                    placeholder='Email'
+                                                    className='border m-2 p-2'
+                                                />
+                                                {formik.errors.email && formik.touched.email && (
+                                                    <div className="text-sm text-red ml-2"> **{formik.errors.email}</div>
+                                                )}
+                                        </div>
 
 
                                     </div>
 
-                                    <div className='w-[60%] flex flex-col justify-end'>
-                                            <label className='ml-2'> Email </label>
-                                            <Field
-                                                name='name'
-                                                value={formik.values.email}
-                                                onChange={formik.handleChange}
-                                                type='text'
-                                                placeholder='Email'
-                                                className='border m-2 p-2'
-                                            />
-                                            {formik.errors.email && formik.touched.email && (
-                                                <div className="text-sm text-red ml-2"> **{formik.errors.email}</div>
-                                            )}
-
-
-                                        </div>
                                 </div>
 
 
 
-                            <button type='submit' className='bg-black mt-3 text-white h-[50px] hover:text-ocean'> Update User </button>
+                            <button type='submit' className='bg-black mt-3 text-white w-full h-[50px] hover:text-ocean'> Update User </button>
 
 
                         </Form>
