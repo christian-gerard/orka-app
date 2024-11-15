9ba3c6b4bc35e2c678212ffe41aad4f8dc7a56dd
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-11-15 14:08:03 -0700

    Updating some stuff

--- server/app/app/asgi.py
+++ server/app/app/asgi.py
@@ -1,10 +1,5 @@
 """
-ASGI config for app project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+ASGI CONFIG
 """
 
 import os
